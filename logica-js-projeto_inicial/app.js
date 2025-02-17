e0e0bb91c1d84e34453646d63057483a01f78553
JunhaumHayden <junhaumhayden@hotmail.com>
2025-02-17 01:52:27 -0300

    Adiciona arquivos iniciais do projeto, incluindo HTML, CSS e lógica JavaScript

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+
+
+let palavraPessoa = "";
+
+if(quantidadePessoas == 1){
+    palavraPessoa = "pessoa";
+}else{
+    palavraPessoa = "pessoas"
+}
+document.querySelector('h1').textContent = "Bem-vindo ao mundo dev";
